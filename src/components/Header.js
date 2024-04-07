53908acdda2959aad558d03e0cd52e2432f07635
Esha Sharma <eshasharmakb22@gmail.com>
2024-04-07 10:26:34 +0530

    designed the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+import logo from "../images/orange.png"
 const Header = () => {
     return (
-        <div>
-            this is the Header
+      <header className="flex justify-between h-18 shadow-md px-8 ">
+        <div className="">
+          <Link to="/" className="flex items-center justify-center">
+                    <img className="w-14" src={logo} alt="logo" />
+                    <span className="m-0 p-0 font-sans font-bold text-base"> Nomadic Nibbles</span>
+          </Link>
         </div>
+      </header>
     );
 }
 
